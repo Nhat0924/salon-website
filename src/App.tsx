3207047cac7574fd5e959f45c38923b7cfffa720
Nhat Nguyen <nhn0001@auburn.edu>
2025-06-27 23:04:45 -0500

    fix broken marker icon

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,20 @@ import facebookSVG from '/src/assets/icons8-facebook.svg'
 import instaSVG from '/src/assets/icons8-instagram.svg'
 import promoImg from '/src/assets/promo.png'
 import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet/dist/leaflet.css';
 
 function App() {
   const mapRef = useRef<HTMLDivElement | null>(null);
+  delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+  });
   useEffect(() => {
   if (!mapRef.current) return;
 
